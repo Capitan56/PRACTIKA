c8ceae7ccb429313336a4e079e3615bc310da003
Capitan56 <70966519+Capitan56@users.noreply.github.com>
2021-07-19 14:51:18 +0300

    Update main.go

--- Practic/main.go
+++ Practic/main.go
@@ -5,29 +5,20 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
 
 func Processor(rw http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		panic(err)
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(rw, err)
 	}
 	fmt.Fprint(rw, len(body))
 }
 
 func main() {
-
-	handler := http.NewServeMux()
-	handler.HandleFunc("/handleHook/Processoring", Processor)
-
-	s := &http.Server{
-		ReadHeaderTimeout: 30 * time.Second,
-		ReadTimeout:       30 * time.Second,
-		WriteTimeout:      30 * time.Second,
-		Handler:           handler,
-		Addr:              "127.0.0.1:3000",
-	}
-	log.Fatal(s.ListenAndServe())
+	http.HandleFunc("/handleHook/Processoring", Processor)
+	err := http.ListenAndServe("127.0.0.1:3000", nil)
+	log.Fatal(err)
 
 }
